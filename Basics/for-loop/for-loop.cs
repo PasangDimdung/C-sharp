789fab6e9d69d2774f8245235d0ad9e6a9aeb204
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 15:47:37 +0545

    added for loop

---
+++ Basics/for-loop/for-loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int [] Numbers = new int [3];
+
+            Numbers[0] = 101;
+            Numbers[1] = 102;
+            Numbers[2] = 103;
+
+            for(int i = 0; i < Numbers.Length; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+
+        }
+    }
+}
