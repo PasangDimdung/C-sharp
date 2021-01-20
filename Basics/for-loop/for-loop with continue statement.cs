fdd41a392c97730492da10daee4d5a7db1b121bb
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 17:27:02 +0545

    added files in for loop

---
+++ Basics/for-loop/for-loop with continue statement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(int i = 1; i <= 20; i++)
+            {
+                //If remainder becomes 1 then it skips rest of the part & continue from the beginning
+                if(i % 2 == 1)
+                    continue;
+
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
