898f66c5eb72f327baf0fece880c905cbd50dbea
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 15:48:59 +0545

    added foreach loop

---
+++ Basics/for-each/for each loop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int [] Numbers = new int [3];
+
+            Numbers[0] = 101;
+            Numbers[1] = 102;
+            Numbers[2] = 103;
+
+           foreach (int i in Numbers)
+           {
+               Console.WriteLine(i);
+           }
+
+        }
+    }
+}
