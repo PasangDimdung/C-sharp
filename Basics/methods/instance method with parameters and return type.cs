44b7e81f374a66522559eb270b674e5625de67f1
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 18:08:47 +0545

    added files in methods

---
+++ Basics/methods/instance method with parameters and return type.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {       
+            Program P = new Program();
+            int Sum = P.AddNumbers(2,4);
+
+            Console.WriteLine("Sum of 2 and 4 is {0}", Sum);
+        }
+
+        //Instance method
+        public int AddNumbers(int FirstNumber, int SecondNumber) 
+        {
+            return FirstNumber + SecondNumber;
+        }
+    }
+}
---
+++ Basics/methods/static method with parameter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {       
+            Program.EvenNumbers(20);
+        }
+
+        //Static method
+        public static void EvenNumbers(int Target) 
+        {
+            int Start = 0;
+
+            while (Start <= Target)
+            {
+                Console.WriteLine(Start);
+
+                Start = Start + 2;
+            }
+        }
+    }
+}
