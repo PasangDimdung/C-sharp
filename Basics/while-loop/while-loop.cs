8b7beb676624af68720b8b8ddee72d01b65efcd2
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 12:03:21 +0545

    added while loop

---
+++ Basics/while-loop/while-loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your target:");
+            int UserTarget = int.Parse(Console.ReadLine());
+
+            int Number = 0;
+
+            while(Number <= UserTarget) {
+                Console.Write(Number + " ");
+                Number = Number + 2;
+            } 
+        }
+    }
+}
