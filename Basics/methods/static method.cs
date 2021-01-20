cb930d86c557d3d1ee2008cf1472c81edd112603
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 17:59:02 +0545

    added file (static method) in method

---
+++ Basics/methods/static method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {       
+            Program.EvenNumbers();
+        }
+
+        //Static method
+        public static void EvenNumbers() 
+        {
+            int Start = 0;
+
+            while (Start <= 20)
+            {
+                Console.WriteLine(Start);
+
+                Start = Start + 2;
+            }
+        }
+    }
+}
