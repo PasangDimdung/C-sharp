4af24a1fd05c39f599cdf60ef0807c4b49b01412
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 17:14:10 +0545

    added files in for loop

---
+++ Basics/for-loop/for-loop with brake statement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+
+                if(i == 10)
+                    break;
+            }
+
+        }
+    }
+}
