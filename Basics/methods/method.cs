18466b5e00709edf76ad40c8f8b7b426c45bb7b6
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-20 17:54:00 +0545

    added methods

---
+++ Basics/methods/method.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Create instance of class Program
+            Program p = new Program();
+
+            p.EvenNumbers();
+        }
+
+        //Instance method
+        public void EvenNumbers() 
+        {
+            int Start = 0;
+
+            while (Start <= 20)
+            {
+                Console.WriteLine(Start);
+
+                Start = Start + 2;
+            }
+        }
+    }
+}
