77e69c26cb514deb47257480dfcad9eed9290990
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-21 08:41:11 +0545

    added files in methods

---
+++ Basics/methods/passing a parameter by reference.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+
+            SimpleMethod(ref i);
+
+            /*
+            Prints 101 because i and j are pointing to the same memory location. 
+            Simple method puts the value 101 to the memory when called.
+            */
+            Console.WriteLine(i);
+        }
+
+        //Passing a parameter by reference
+        public static void SimpleMethod(ref int j)
+        {
+            j = 101;
+        }
+    }
+}
---
+++ Basics/methods/passing a parameter by value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {       
+            int i = 0;
+
+            SimpleMethod(i);
+
+            //Prints zero because i and j are pointing in different memory location
+            Console.WriteLine(i);
+        }
+
+        //Passing a parameter by value
+        public static void SimpleMethod(int j) 
+        {
+            j = 101;
+        }
+    }
+}
