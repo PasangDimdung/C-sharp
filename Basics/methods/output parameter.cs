51a16a62a54c5e3277eecd3f30f7c388249893d8
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-21 08:58:50 +0545

    added files in methods

---
+++ Basics/methods/output parameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int Total = 0;
+            int Product = 0;
+
+            Calculate(5,2, out Total, out Product);
+
+            Console.WriteLine("Sum is {0} and Product is {1}", Total, Product);
+        }
+
+        //Output parameter
+        public static void Calculate( int FirstNumber, int SecondNumber, out int Sum, out int Product)
+        {
+            Sum = FirstNumber + SecondNumber;
+            Product = FirstNumber * SecondNumber;
+        }
+    }
+}
