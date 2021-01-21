b82b56a9bbee4b22f5db7a4ed5042123c9e11332
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-21 16:37:57 +0545

    added file(static constructor) in class

---
+++ Basics/class/static constructor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Circle
+{
+
+    //Static keyword is used to make class member static. It is made public because Class Program can access it 
+    public static float _PI;
+    int _Radius;
+
+    //Static constructor are called before instance construtor and even before any static fields
+    static Circle()
+    {
+        Console.WriteLine("Static constructor called..");
+        Circle._PI = 3.141F;
+    }
+    public Circle(int Radius)
+    {
+        this._Radius = Radius;
+    }
+
+    public float CalculateArea()
+    {
+        return Circle._PI * this._Radius * this._Radius;
+    }
+
+}
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine(Circle._PI);
+    }
+
+}
