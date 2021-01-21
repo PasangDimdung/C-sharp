8dca1c48b1cc3c6600d771bf0bd5851943edd9e3
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-21 10:47:18 +0545

    added namespaces

---
+++ Basics/namespaces/namespaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using PATA=  ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
+
+class Program
+{
+    public static void Main()
+    {
+        PATA.ClassA.Print();
+        PATB.ClassA.Print();
+    }
+}
+
+//Namespace organizes program and asist in avoiding name clashes
+namespace ProjectA 
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A print method");
+            }
+        }
+    }
+}
+
+namespace ProjectA 
+{
+    namespace TeamB
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team B print method");
+            }
+        }
+    }
+}
