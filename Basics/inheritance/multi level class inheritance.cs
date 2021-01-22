cb96a22f8a98961c8af7168fe0135cd001bc3991
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-22 09:39:00 +0545

    added file in inheritance

---
+++ Basics/inheritance/multi level class inheritance.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+//Employee class is base class which contains common fields
+public class Employee
+{
+    public string FirstName;
+    public string LastName;
+
+    public void PrintFullName()
+    {
+        Console.WriteLine(FirstName + " " + LastName);
+    }
+}
+
+//FullTimeEmployee is derived class which inheritates Employee class
+public class FullTimeEmployee: Employee
+{
+    public int YearlyRate;
+}
+
+public class PartTimeEmployee: Employee
+{
+    public int HourlyRate;
+}
+
+//Class A derives from the derived class PartTimeEmployee
+public class A : PartTimeEmployee
+{
+
+}
+class Program
+{
+    public static void Main()
+    {
+        //C# supports multi level class inheritance
+        A A = new A();
+        A.FirstName = "Multi level";
+        A.LastName = "Inheritance";
+        A.HourlyRate = 1;
+
+
+        FullTimeEmployee FTE = new FullTimeEmployee();
+        FTE.FirstName = "Pasang";
+        FTE.LastName = "Dimdung";
+        FTE.YearlyRate = 40000000;
+        FTE.PrintFullName();
+
+        PartTimeEmployee PTE = new PartTimeEmployee();
+        PTE.FirstName = "Bishal";
+        PTE.LastName = "Dimdung";
+        PTE.HourlyRate = 20000;
+        PTE.PrintFullName();
+    }
+    
+
+}
