171216603be7ef24847cb7b18fbef4b9e816160a
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-22 15:25:30 +0545

    added file in methods

---
+++ Basics/methods/method overloading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Program.Add( 2.24F, 5 );
+    }
+
+    //Method overloading
+    public static void Add(int FN, int SN)
+    {
+        Console.WriteLine("Sum = {0}", FN + SN);
+    }
+
+     public static void Add(float FN, int SN)
+    {
+        Console.WriteLine("Sum = {0}", FN + SN);
+    }
+
+}
