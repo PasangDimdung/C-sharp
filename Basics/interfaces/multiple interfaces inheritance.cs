e91a84b8a0c5def56f313800b17f86dbe65b33a5
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-23 15:19:40 +0545

    added file in interfaces

---
+++ Basics/interfaces/multiple interfaces inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+interface ICustomer
+{
+    void Print();
+}
+
+interface I2
+{
+    void I2Method();
+}
+
+//A class can inherit multiple interfaces
+public class Customer : ICustomer, I2
+{
+    public void Print()
+    {
+        Console.WriteLine("This is interface print method");
+    }
+
+    public void I2Method()
+    {
+        Console.WriteLine("This is I2 method");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer();
+        C1.Print();
+        C1.I2Method();
+    }
+}
