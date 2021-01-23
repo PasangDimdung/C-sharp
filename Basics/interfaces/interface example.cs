281f18c40731d31c3bb3754452ab93acbd259762
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-23 19:09:37 +0545

    added file in interface

---
+++ Basics/interfaces/interface example.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+interface ICustomer1
+{
+    void Print1();
+}
+
+interface ICustomer2 : ICustomer1
+{
+    void Print2();
+}
+
+public class Customer : ICustomer2
+{
+    public void Print1()
+    {
+        Console.WriteLine("Print 1 method");
+    }
+
+    public void Print2()
+    {
+        Console.WriteLine("Print 2 method");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        /* We cannot create instance of an interface because interface only have declarations which makes no sense
+        to create an object of an interface. 
+        But an interface reference variable can point to a derived class object. */
+        ICustomer1 C1 = new Customer();
+        C1.Print1();
+    }
+}
