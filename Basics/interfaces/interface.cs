0eaeeedcf708a45aefc3ce3494463d9eed9c46c6
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-23 10:46:31 +0545

    added interface

---
+++ Basics/interfaces/interface.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+interface ICustomer
+{
+    //Access modifier is pubic by default in interfaces. Interface contains only declaration, no definition(implementation)
+    void Print();
+}
+public class Customer : ICustomer
+{
+    public void Print()
+    {
+        Console.WriteLine("This is interface print method");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Customer C1 = new Customer();
+        C1.Print();
+    }
+}
