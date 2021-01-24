9b0a11418fd2122823f6dce91297a90a03149fb3
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-24 10:38:46 +0545

    added file in inheritance

---
+++ Basics/inheritance/multiple class inheritance problem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class A
+{
+    public virtual void Print()
+    {
+        Console.WriteLine("Print");
+    }
+}
+
+class B : A
+{
+    public override void Print()
+    {
+        Console.WriteLine("Class B Print Method");
+    }
+}
+
+class C : A
+{
+    public override void Print()
+    {
+        Console.WriteLine("Class C Print Method");
+    }
+}
+
+//Multiple class inheritance is not possible in C#
+class D : A, B
+{
+
+}
+
+
+class Program
+{
+    public static void Main()
+    {
+        D C = new D();
+
+        /*When D calls a method, B and C have overriden the method differently then from which class does it inherit?
+        This ambiguity is called Diamond Problem. */
+        D.Print();
+    }
+}
