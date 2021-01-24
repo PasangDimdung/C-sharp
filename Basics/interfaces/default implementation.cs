b4378f8be1dec0127ff70d4c68b75f0027b0ceff
PasangDimdung <pasangdimdung045@gmail.com>
2021-01-24 09:44:53 +0545

    added file in interface

---
+++ Basics/interfaces/default implementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+interface ICustomer1
+{
+    void Print();
+}
+
+interface ICustomer2
+{
+    void Print();
+}
+
+public class Customer : ICustomer1, ICustomer2
+{
+
+    //If you want to make your method default. Implement it normally and other methods explicitly
+    public void Print()
+    {
+        Console.WriteLine("Interface 1 Print method");
+    }
+
+    void ICustomer2.Print()
+    {
+        Console.WriteLine("Interface 2 Print method");
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Customer C = new Customer();
+
+        //Accessing default interface method
+        C.Print();
+    }
+}
