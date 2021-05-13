cc908a76ecd1ec3860a49020ce6892ad93f9b991
PasangDimdung <pasangdimdung045@gmail.com>
2021-05-13 18:48:23 +0545

    added func delegate

---
+++ Basics/func-delegate/FuncDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FuncDelegate
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Func<int, int, string> funcDelegate = (firstNumber, secondNumber) => "Sum = " + (firstNumber + secondNumber).ToString();
+
+            string result = funcDelegate(10, 20);
+            Console.WriteLine(result);
+        }
+    }
+}
