98c36dc94f381142e070fff6756e5b3e5d863784
PasangDimdung <pasangdimdung045@gmail.com>
2021-05-31 10:16:40 +0545

    added abstract class

---
+++ Basics/abstract-class/AbstractClasses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    //abstract classes can only be used as base class
+    public abstract class Print
+    {
+        //abstract classes may contain abstract members. cannot have implementation
+        public abstract void  PrintFullName();
+
+        //abstract classes may contain non-abstract members
+        public void PrintRollNo()
+        {
+            Console.WriteLine("RollNo");
+        }
+    }
+    class Program : Print
+    {
+        static void Main()
+        {
+            Program P = new Program();
+            P.PrintFullName();
+            P.PrintRollNo();
+        }
+
+        public override void PrintFullName()
+        {
+            Console.WriteLine("FullName");
+        }
+    }
+}
