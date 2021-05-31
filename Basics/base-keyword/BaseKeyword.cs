f0449ca82517256fb2e04129f4d7f795eafa1c08
PasangDimdung <pasangdimdung045@gmail.com>
2021-05-31 19:53:00 +0545

    added base reserved keyword

---
+++ Basics/base-keyword/BaseKeyword.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BaseKeyword
+{
+    abstract class Person
+    {
+        private readonly string _firstname;
+        private readonly string _lastname;
+
+        protected Person(string firstname, string lastname)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+        }
+        public string GetFullName()
+        {
+            return $"{_firstname} {_lastname}";
+        }
+    }
+
+    class Pasang: Person
+    {
+        public Pasang(string firstname, string lastname): base(firstname,lastname)
+        {
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person P = new Pasang("Pasang", "Dimdung");
+            Console.WriteLine(P.GetFullName());
+        }
+
+    }
+}
