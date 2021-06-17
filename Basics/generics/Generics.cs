2e0de3bfcde500fc607360c43ce583b20e6044c5
PasangDimdung <pasangdimdung045@gmail.com>
2021-06-17 15:56:50 +0545

    addded generics

---
+++ Basics/generics/Generics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var number = new Nullable<int>(20);
+            Console.WriteLine("has value? : " + number.HasValue);
+            Console.WriteLine("value: " + number.GetValueOrDefault());
+        }
+    }
+
+    //where T: struct is generic constraints
+    public class Nullable<T> where T : struct
+    {
+        private object _value;
+
+        public Nullable()
+        {
+                
+        }
+
+        public Nullable(T value)
+        {
+            _value = value;
+        }
+
+        public bool HasValue
+        {
+            get { return _value != null; }
+        }
+
+        public T GetValueOrDefault()
+        {
+            if (HasValue)
+                return (T)_value;
+            return default(T);
+        }
+
+
+    }
+}
