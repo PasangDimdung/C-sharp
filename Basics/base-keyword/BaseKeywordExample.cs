4fdd25c1b019440e17dc90e7e92b6c62032b85fb
PasangDimdung <pasangdimdung045@gmail.com>
2021-06-18 11:20:35 +0545

    added basekeyword eg

---
+++ Basics/base-keyword/BaseKeywordExample.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BaseClassDemo
+{
+    public class BaseClass
+    {
+        int num;
+
+        public BaseClass()
+        {
+            Console.WriteLine("in BaseClass()");
+        }
+
+        //overloaded constructor
+        public BaseClass(int i)
+        {
+            num = i;
+            Console.WriteLine("in BaseClass(int i): " + i);
+        }
+
+        public int GetNum()
+        {
+            return num;
+        }
+    }
+
+    //base keyword specify which base-class constructor should be called when creating instances of derived class
+    public class DerivedClass : BaseClass
+    {
+        // This constructor will call BaseClass.BaseClass()
+        public DerivedClass() : base()
+        {
+        }
+
+        // This constructor will call BaseClass.BaseClass(int i)
+        public DerivedClass(int i) : base(i)
+        {
+        }
+
+        static void Main()
+        {
+            DerivedClass md = new DerivedClass();
+            DerivedClass md1 = new DerivedClass(125);
+        }
+    }
+}
